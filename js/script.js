9f54e44a747c43f9cf711f257ec18abca7b82827
Anna Kuszal <anna.kuszal@o2.pl>
2019-10-21 19:18:14 +0200

    Add articles to links & add .active to articles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log (event);
@@ -12,7 +13,7 @@ const titleClickHandler = function(event){
     activeLink.classList.remove('active');
     }
   
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
   
@@ -23,11 +24,17 @@ const titleClickHandler = function(event){
       activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
+    /* [DONE] get 'href' attribute from the clicked link */
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
   
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+    targetArticle.classList.add('active');
+
   }
   
   const links = document.querySelectorAll('.titles a');
