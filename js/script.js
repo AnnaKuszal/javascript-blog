4106a3355ecd952776985d404ee4cf372c170b67
Anna Kuszal <anna.kuszal@o2.pl>
2019-10-23 20:24:27 +0200

    Fix errors in js & scss code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 {
-'use strict';
+  'use strict';
 
   const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log (event);
-  
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
-    activeLink.classList.remove('active');
+      activeLink.classList.remove('active');
     }
-  
+
     /* [DONE] add class 'active' to the clicked link */
     console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
-  
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('article.active');
 
@@ -28,7 +28,7 @@
     /* [DONE] get 'href' attribute from the clicked link */
     const articleSelector = clickedElement.getAttribute('href');
     console.log(articleSelector);
-  
+
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(articleSelector);
     console.log(targetArticle);
@@ -36,8 +36,8 @@
     /* [DONE] add class 'active' to the correct article */
     targetArticle.classList.add('active');
 
-  }
-  
+  };
+
   /* PRZENOSIMY KOD NA KONIEC NOWEJ FUNKCJI
 
   const links = document.querySelectorAll('.titles a');
@@ -48,8 +48,8 @@
   */
 
   const optArticleSelector = '.post',
-  optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+    optTitleSelector = '.post-title',
+    optTitleListSelector = '.titles';
 
   function generateTitleLinks(){
     console.log('Wygenerowano listę linków!');
@@ -69,7 +69,7 @@
       /* get the article id */
       const articleId = article.getAttribute('id');
       console.log(articleId);
-        
+
       /* find the title element */
       /* get the title from the title element */
       const articleTitle = article.querySelector(optTitleSelector).innerHTML;
@@ -83,7 +83,7 @@
       /* insert link into titleList */
       //titleList.innerHTML = titleList.innerHTML + linkHTML;  kod przed optymalizacją
       //titleList.insertAdjacentHTML('beforeend', linkHTML);  // kod po optymalizacji
-    
+
       /* insert link into html variable */
       html = html + linkHTML;
 
